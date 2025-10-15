062e20a09f69b1c9608809666e9791dc6df2b409
Maxim <maxim2266@outlook.com>
2025-10-15 18:42:02 +0100

    Script runner updated with help string and STDIN processing.

--- run.lua
+++ run.lua
@@ -1,14 +1,38 @@
--- parameter check
-if not arg[1] then
-	app:fail("missing script to run")
-end
+-- help string
+local HELP <const> = [=[Usage:
+ ]=] .. app.NAME .. [=[ script [args...]
+    Run Lua script with app.lua runtime included. Script name "-" instructs
+    to read the script from STDIN.
+ ]=] .. app.NAME .. [=[ -h/--help
+    Display this help and exit.
+]=]
 
--- shift arguments
-table.move(arg, 1, #arg, 0)
-arg[#arg] = nil
+app:run(function()
+	-- script name
+	local script <const> = arg[1]
 
--- update application name
-app.NAME = arg[0]:match("[^/]+$")
+	-- parameter check
+	if not script then
+		app:fail("missing script name")
+	end
 
--- run the script
-app:run(dofile, arg[0])
+	if script == "-h" or script == "--help" then
+		io.stderr:write(HELP)
+		os.exit(false)
+	end
+
+	-- shift arguments
+	table.move(arg, 1, #arg, 0)
+	arg[#arg] = nil
+
+	-- STDIN
+	if script == "-" then
+		return dofile()
+	end
+
+	-- update application name
+	app.NAME = script:match("[^/]+$")
+
+	-- run
+	return dofile(script)
+end)
